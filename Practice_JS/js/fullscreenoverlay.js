faf6779a2f014a7084582a57783837a8d865f30c
ImNotAntMan <ikarusppong@gmail.com>
2021-12-07 20:08:22 +0900

    2021/12/07 잘 안돼서 베끼니 잘 됨...무슨 일일까??? 걍 지나가리~~

--- Practice_JS/js/fullscreenoverlay.js
+++ Practice_JS/js/fullscreenoverlay.js
@@ -1,7 +1,9 @@
 function openNav() {
-    document.getElementById("myNav").style.width = "100%";
+    // document.getElementById("myNav").style.width = "100%"; // Slie in from the side
+    document.getElementById("myNav").style.height = "100%";
 }
 
 function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
+    // document.getElementById("myNav").style.width = "0%"; // Slie in from the side
+    document.getElementById("myNav").style.height = "0%"; // Slide in from the top
 }
