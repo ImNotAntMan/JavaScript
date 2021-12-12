b52910031539495bc88a7f9ef6f303e0726ee6e4
ImNotAntMan <ikarusppong@gmail.com>
2021-12-13 00:31:20 +0900

    2021/12/13 Update at Home with NoteBook

--- Site20211210/js/index.js
+++ Site20211210/js/index.js
@@ -6,3 +6,27 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+// Get the modal
+var modal = document.getElementById("myImgModal");
+var myimg = document.getElementsByClassName("myimg");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+console.log(myimg.item(1).currentSrc);
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  console.log(modalImg.src);
+  captionText.innerHTML = this.alt;
+
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
